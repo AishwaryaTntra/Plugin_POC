088d8bc6d6ef0ecd80d933ec6208347d00a16f21
AishwaryaTntra <aishwarya.pakeyara@tntra.io>
2023-02-01 11:40:20 +0530

    commente required files

--- lib/accounts.rb
+++ lib/accounts.rb
@@ -4,10 +4,10 @@ require 'accounts/railtie'
 # module Accounts
 #   # Your code goes here...
 # end
-require 'app/models/accounts/*'
-require 'app/controllers/accounts/*'
-require 'app/views/accounts/*'
-require 'config/routes'
+# require 'app/models/accounts/*'
+# require 'app/controllers/accounts/*'
+# require 'app/views/accounts/*'
+# require 'config/routes'
 
 Accounts::MIGRATION_BASE_CLASS = if ActiveRecord::VERSION::MAJOR >= 5
                                    ActiveRecord::Migration[5.0]
