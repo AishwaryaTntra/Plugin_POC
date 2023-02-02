42cd749a27e75cf164c4cdfa7dd932347d8bee78
AishwaryaTntra <aishwarya.pakeyara@tntra.io>
2023-02-02 15:34:40 +0530

    controller generator

--- lib/generators/accounts/accounts_generator.rb
+++ lib/generators/accounts/accounts_generator.rb
@@ -1,34 +1,34 @@
-# frozen_string_literal: true
+# # frozen_string_literal: true
 
-require 'rails/generators/base'
+# require 'rails/generators/base'
 
-module Generators
-  # lib > generators > accounts > accounts_generator
-  class AccountsGenerator < Rails::Generators::NamedBase
-    include Rails::Generators::ResourceHelpers
+# module Generators
+#   # lib > generators > accounts > accounts_generator
+#   class AccountsGenerator < Rails::Generators::NamedBase
+#     include Rails::Generators::ResourceHelpers
 
-    MODEL_NAMES = %i[basic_group group sub_group account_master].freeze
-    namespace 'accounts'
-    source_root File.expand_path('../templates', __dir__)
+#     MODEL_NAMES = %i[basic_group group sub_group account_master].freeze
+#     namespace 'accounts'
+#     source_root File.expand_path('../templates', __dir__)
 
-    def create_models
-      MODEL_NAMES.each do |model_name|
-        invoke 'active_record:model', model_name, migration: false
-      end
-    end
+#     def create_models
+#       MODEL_NAMES.each do |model_name|
+#         invoke 'active_record:model', model_name, migration: false
+#       end
+#     end
 
-    desc 'accounts routes'
-    hook_for :orm, required: true
+#     desc 'accounts routes'
+#     hook_for :orm, required: true
 
-    class_option :routes, desc: 'Generates routes', type: :boolean, default: true
+#     class_option :routes, desc: 'Generates routes', type: :boolean, default: true
 
-    def add_accounts_routes
-      account_routes = "scope module: 'accounts' do ".dup
-      %w[basic_groups groups sub_groups account_masters].each do |class_name|
-        account_routes << %(" resources :#{class_name} ")
-      end
-      account_routes << 'end'
-      route account_routes
-    end
-  end
-end
+#     def add_accounts_routes
+#       account_routes = "scope module: 'accounts' do ".dup
+#       %w[basic_groups groups sub_groups account_masters].each do |class_name|
+#         account_routes << %(" resources :#{class_name} ")
+#       end
+#       account_routes << 'end'
+#       route account_routes
+#     end
+#   end
+# end
---
+++ lib/generators/templates/sub_groups_controller.rb
@@ -0,0 +1,62 @@
+# frozen_string_literal: true
+
+module Accounts
+  # app > controllers > accounts > sub_groups_controller
+  class SubGroupsController < ::Accounts::ApplicationController
+    before_action :set_accounts_sub_group, only: %i[show edit update destroy]
+
+    # GET /accounts/sub_groups
+    def index
+      @accounts_sub_groups = Accounts::SubGroup.all
+    end
+
+    # GET /accounts/sub_groups/1
+    def show; end
+
+    # GET /accounts/sub_groups/new
+    def new
+      @accounts_sub_group = Accounts::SubGroup.new
+    end
+
+    # GET /accounts/sub_groups/1/edit
+    def edit; end
+
+    # POST /accounts/sub_groups
+    def create
+      @accounts_sub_group = Accounts::SubGroup.new(accounts_sub_group_params)
+
+      if @accounts_sub_group.save
+        redirect_to @accounts_sub_group, notice: 'Sub group was successfully created.'
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
+
+    # PATCH/PUT /accounts/sub_groups/1
+    def update
+      return unless @accounts_sub_group.update(accounts_sub_group_params)
+
+      redirect_to @accounts_sub_group, notice: 'Sub group was successfully updated.'
+      elseu
+      render :edit, status: :unprocessable_entity
+    end
+
+    # DELETE /accounts/sub_groups/1
+    def destroy
+      @accounts_sub_group.destroy
+      redirect_to accounts_sub_groups_url, notice: 'Sub group was successfully destroyed.'
+    end
+
+    private
+
+    # Use callbacks to share common setup or constraints between actions.
+    def set_accounts_sub_group
+      @accounts_sub_group = Accounts::SubGroup.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def accounts_sub_group_params
+      params.require(:accounts_sub_group).permit(:name, :basic_group_name)
+    end
+  end
+end
