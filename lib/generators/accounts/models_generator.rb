adbae70ff74ffa4454cfdfdf7b59ccb002169b83
AishwaryaTntra <aishwarya.pakeyara@tntra.io>
2023-02-03 16:06:46 +0530

    model templates and generator

--- lib/generators/accounts/models_generator.rb
+++ lib/generators/accounts/models_generator.rb
@@ -7,11 +7,11 @@ module Accounts
   class ModelsGenerator < Rails::Generators::Base
     MODELS = %w[account_master basic_group group sub_group].freeze
     desc 'Generates models for accounting module'
-    source_root File.expand_path('../templates/models', __dir__)
+    source_root File.expand_path('../templates/models/accounts', __dir__)
 
     def create_migration_file
       MODELS.each do |model_name|
-        template model_name.to_s, "app/models/accounts/#{model_name}.rb"
+        template "#{model_name}.rb", "app/models/accounts/#{model_name}.rb"
       end
     end
   end
---
+++ lib/generators/templates/models/accounts/account_master.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+module Accounts
+  class AccountMaster < ActiveRecord::Base
+  end
+end
